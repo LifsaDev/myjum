3f4626b18531e1790482be5220f38f0080da765d
Alpha Emmanuel <65494407+ealpha072@users.noreply.github.com>
2020-09-10 23:05:34 +0300

    Initial js file

---
+++ index.js
@@ -0,0 +1,27 @@
+/*let src = ['images/alcohol.jpg','images/detegent.jpg','images/fashion.jpg','images/home.jpg','images/phone.jpg','images/shoe.jpg'];
+
+//constants
+const img = document.createElement('img');
+const dropdown = document.getElementsByClassName('dropdown');
+
+setInterval(function(){
+    img.setAttribute('src',src[Math.floor(Math.random()*src.length)])
+},5000)
+document.querySelector('.categoryresults').appendChild(img);*/
+
+function dropDown(){
+    document.getElementById('myDropDown').classList.toggle("show");
+}
+
+window.onclick =(e)=>{
+    if(!e.target.matches('.dropbtn')){
+        var dropdowns = document.getElementsByClassName('dropdown-content');
+        var i;
+        for(i=0;i<dropdowns.length;i++){
+            var openDropDown = dropdowns[i];
+            if(openDropDown.classList.contains('show')){
+                openDropDown.classList.remove('show')
+            }
+        }
+    }
+}
