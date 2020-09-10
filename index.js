e4d38103a2c981d37bdc5c23554cd037edc2794b
Alpha Emmanuel <65494407+ealpha072@users.noreply.github.com>
2020-09-10 23:15:58 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
-/*let src = ['images/alcohol.jpg','images/detegent.jpg','images/fashion.jpg','images/home.jpg','images/phone.jpg','images/shoe.jpg'];
+let src = ['images/alcohol.jpg','images/detegent.jpg','images/fashion.jpg','images/home.jpg','images/phone.jpg','images/shoe.jpg'];
 
 //constants
 const img = document.createElement('img');
 const dropdown = document.getElementsByClassName('dropdown');
 
 setInterval(function(){
-    img.setAttribute('src',src[Math.floor(Math.random()*src.length)])
+    img.setAttribute('src',src[Math.floor(Math.random()*src.length)]);
+    document.querySelector('.categoryresults').appendChild(img);
 },5000)
-document.querySelector('.categoryresults').appendChild(img);*/
+
 
 function dropDown(){
     document.getElementById('myDropDown').classList.toggle("show");
