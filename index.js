2b8462030513b17b54513cc1a0f21eba055958e0
Alpha Emmanuel <65494407+ealpha072@users.noreply.github.com>
2020-09-11 14:30:03 +0300

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const img = document.createElement('img');
 const dropdown = document.getElementsByClassName('dropdown');
 
 setInterval(function(){
-    img.setAttribute('src',src[Math.floor(Math.random()*src.length)]);
+    img.setAttribute('src',src[Math.floor(Math.random()*src.length)])
     document.querySelector('.categoryresults').appendChild(img);
 },5000)
 
@@ -14,7 +14,8 @@ function dropDown(){
     document.getElementById('myDropDown').classList.toggle("show");
 }
 
-window.onclick =(e)=>{
+window.addEventListener('click',(e)=>{
+    e.preventDefault();
     if(!e.target.matches('.dropbtn')){
         var dropdowns = document.getElementsByClassName('dropdown-content');
         var i;
@@ -25,4 +26,24 @@ window.onclick =(e)=>{
             }
         }
     }
+})
+
+
+function helpDropDown(){
+    document.getElementById('helpdropdown').classList.toggle("showmore")
 }
+
+
+ window.addEventListener('click',(e)=>{
+    e.preventDefault();
+    if(!e.target.matches('.droplink')){
+        var dropdown2 =document.getElementsByClassName('dropdown');
+        var i;
+        for(i=0;i<dropdown2.length;i++){
+            var openlink = dropdown2[i];
+            if(openlink.classList.contains('showmore')){
+                openlink.classList.remove('showmore')
+            }
+        }
+    } 
+ })
