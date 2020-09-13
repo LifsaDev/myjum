c9070bbebd22ac2a3de60b133c2222251bb51658
Alpha Emmanuel <65494407+ealpha072@users.noreply.github.com>
2020-09-13 16:32:19 +0300

    Add files via upload

---
+++ ecommerce/php/cart.php
@@ -0,0 +1,11 @@
+<?php include 'header.php' ;?>
+    <div class="cart">
+        <img src="../index/images/empty-cart.png">
+        <h2>Your cart is empty!</h2>
+        <p>Already have an account?<a href="">Login</a></p>
+        <button onclick="location.href='../index/index.html'">START SHOPPING</button>
+    </div>
+<?php include 'footer.php' ?>
+    <script src="../js/drops.js"></script>
+</body>
+</html>
